cacbca537ed089391d6445a0e2ab6a85b08e610b
czl <849442100@qq.com>
2016-11-21 11:35:35 +0800

    11-21上午

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var router = express.Router();
+//加载express的路由模块
+var models=require('../db/model');
+var markdown = require('markdown').markdown;
 
+
+var router = express.Router();
+//指向首页
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //populate("user")把user属性转换为对象  exec执行匿名函数
+  models.Article.find({}).populate('user').exec(function (err,articles) {
+    // console.log(articles);
+    articles.forEach(function (article) {
+      article.content = markdown.toHTML(article.content);
+    });
+    res.render('index', { title: '博客',articles:articles });
+  });
+
 });
 
 module.exports = router;
